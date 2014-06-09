b4009b07e25db5588e25484024338812bedaf743
My Ho <mydiemho@gmail.com>
2014-06-09 01:09:43 -0700

    Feature2: User can select custom tip percentage if desired

--- TipCalculator/app/src/main/java/com/codepath/example/tipcalculator/app/TipCalculatorActivity.java
+++ TipCalculator/app/src/main/java/com/codepath/example/tipcalculator/app/TipCalculatorActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +22,10 @@ public class TipCalculatorActivity extends Activity {
     private Button btnOkService;
     private Button btnGoodService;
     private Button btnGreatService;
-    private Button btnPressed;
+    private int tipPercentage;
+    private SeekBar sbTipPercentage;
+    private TextView tvTipPercentageLabel;
+    private TextView tvTotalAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +37,46 @@ public class TipCalculatorActivity extends Activity {
         btnOkService = (Button) findViewById(R.id.btnOkService);
         btnGoodService = (Button) findViewById(R.id.btnGoodService);
         btnGreatService = (Button) findViewById(R.id.btnGreatService);
+        sbTipPercentage = (SeekBar) findViewById(R.id.sbTipPercentage);
+        tvTipPercentageLabel = (TextView) findViewById(R.id.tvTipPercentageLabel);
+        tvTotalAmount = (TextView) findViewById(R.id.tvTotalAmount);
 
         setUpButtonClickListener();
         setUpInputChangedListener();
+        setUpTipSliderChangedListener();
+    }
+
+    private void setUpTipSliderChangedListener() {
+        sbTipPercentage.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                // Display error message when trying to calculate missing input.
+                String strInputAmount = etInputAmount.getText().toString();
+                if (strInputAmount.isEmpty()) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Please enter an amount", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    tvTipAmount.setText("");
+                    // return slider to its previous position
+                    seekBar.setProgress(tipPercentage);
+                    return;
+                }
+
+                tipPercentage = seekBar.getProgress();
+                tvTipPercentageLabel.setText(String.valueOf(progress) + "%");
+                updateTip();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 
     private void setUpButtonClickListener() {
@@ -45,6 +85,7 @@ public class TipCalculatorActivity extends Activity {
             public void onClick(View v) {
                 btnGoodService.setEnabled(true);
                 btnGreatService.setEnabled(true);
+                tipPercentage = Integer.parseInt(v.getTag().toString());
                 calculateTip(v);
             }
         });
@@ -52,6 +93,7 @@ public class TipCalculatorActivity extends Activity {
         btnGoodService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                tipPercentage = Integer.parseInt(v.getTag().toString());
                 btnOkService.setEnabled(true);
                 btnGreatService.setEnabled(true);
                 calculateTip(v);
@@ -61,6 +103,7 @@ public class TipCalculatorActivity extends Activity {
         btnGreatService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                tipPercentage = Integer.parseInt(v.getTag().toString());
                 btnOkService.setEnabled(true);
                 btnGoodService.setEnabled(true);
                 calculateTip(v);
@@ -72,7 +115,6 @@ public class TipCalculatorActivity extends Activity {
         etInputAmount.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
@@ -82,7 +124,6 @@ public class TipCalculatorActivity extends Activity {
 
             @Override
             public void afterTextChanged(Editable s) {
-
             }
         });
     }
@@ -98,44 +139,33 @@ public class TipCalculatorActivity extends Activity {
             return;
         }
 
-        double inputAmount;
-        try {
-            inputAmount = Double.parseDouble(etInputAmount.getText().toString());
-        } catch (NumberFormatException e) {
-            Toast toast = Toast.makeText(getApplicationContext(), String.format("Invalid input: %s", etInputAmount.getText().toString()), Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.CENTER, 0, 0);
-            toast.show();
-            return;
-        }
-
-        btnPressed = (Button) v;
+        // disabled button once it's pressed
         v.setEnabled(false);
-        double percentage = Double.parseDouble(v.getTag().toString());
-        double tipAmount = inputAmount * percentage;
+
+        // change slider to reflect button pressed
+        sbTipPercentage.setProgress(tipPercentage);
+
+        double inputAmount = Double.parseDouble(etInputAmount.getText().toString());
+        double tipAmount = inputAmount * (tipPercentage / 100.0);
         tvTipAmount.setText(formatter.format(tipAmount));
+        tvTotalAmount.setText(formatter.format(tipAmount + inputAmount));
     }
 
     public void updateTip() {
-        if (btnPressed != null) {
+        // only update tip amount after input change if one of the button is clicked
+        // or if the slider is changed to a nonzero value.
+        if (tipPercentage != 0) {
             String strInputAmount = etInputAmount.getText().toString();
             if (strInputAmount.isEmpty()) {
                 tvTipAmount.setText("");
                 return;
             }
 
-            double inputAmount;
-            try {
-                inputAmount = Double.parseDouble(etInputAmount.getText().toString());
-            } catch (NumberFormatException e) {
-                Toast toast = Toast.makeText(getApplicationContext(), String.format("Invalid input: %s", etInputAmount.getText().toString()), Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-                return;
-            }
-
-            double percentage = Double.parseDouble(btnPressed.getTag().toString());
-            double tipAmount = inputAmount * percentage;
+            double inputAmount = Double.parseDouble(etInputAmount.getText().toString());
+            double tipAmount = inputAmount * (tipPercentage / 100.0);
             tvTipAmount.setText(formatter.format(tipAmount));
+            tvTotalAmount.setText(formatter.format(tipAmount + inputAmount));
+
         }
     }
 
